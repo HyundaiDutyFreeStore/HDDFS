edf00a3c3bf3188afd1ead300f4cbbeb211b9133
kcj1284 <kcj1284@gmail.com>
2023-01-24 15:40:39 +0900

    [#18-1] 면세점 혼잡도 csv 읽어오는 방식 변경

--- src/main/java/com/hyundai/dutyfree/vo/PedestrianVO.java
+++ src/main/java/com/hyundai/dutyfree/vo/PedestrianVO.java
@@ -1,24 +1,20 @@
 package com.hyundai.dutyfree.vo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * PedestrianVO
  * 
  * @author 김찬중
- * @since 01.09
+ * @since 01.21
  * 
  *        <pre>
  * 수정일                 수정자                         수정내용
  * ----------  ---------------    ---------------------------
- * 2023.01.09    김찬중                         최초 생성
+ * 2023.01.21    김찬중                         최초 생성
  *        </pre>
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class PedestrianVO {
 	private int count; // counting 된 사람의 수
 
