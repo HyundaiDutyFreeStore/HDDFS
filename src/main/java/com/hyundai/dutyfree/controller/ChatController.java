d64902b38d80a8fecf99314fc210ea5a0cc86ed7
gahee3839 <gahee3839@naver.com>
2023-01-24 20:26:32 +0900

    [#30]채팅기본세팅

--- src/main/java/com/hyundai/dutyfree/controller/ChatController.java
+++ src/main/java/com/hyundai/dutyfree/controller/ChatController.java
@@ -1,21 +1,26 @@
 package com.hyundai.dutyfree.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hyundai.dutyfree.security.domain.CustomUser;
+import com.hyundai.dutyfree.vo.MemberVO;
+
 import lombok.extern.log4j.Log4j;
+
 /**
  * ChatController
  * 
  * @author 김가희
  * @since 01.24
  * 
- *        
- * 수정일                 수정자                              수정내용
- * ----------  ---------------  ---------------------------
- * 2023.01.24   김가희                         최초 생성
- *        
+ * 
+ *        수정일 수정자 수정내용 ---------- --------------- ---------------------------
+ *        2023.01.24 김가희 최초 생성
+ * 
  */
 @Controller
 @Log4j
@@ -25,5 +30,17 @@ public class ChatController {
 	public void start() {
 		log.info("챗봇시작화면");
 	}
+
+	@GetMapping("/chat")
+	public void chat(Model model) {
+
+		MemberVO user = (MemberVO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		log.info("==================================");
+		log.info("@ChatController, GET Chat / Username : " + user.getMname());
+
+		model.addAttribute("userid", user.getMid());
+	}
+	
 
 }
