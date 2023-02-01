b7a8b765d78dc06dca32041deb13cb6eb22e7330
jin-su-park <gary3545@naver.com>
2023-02-02 00:07:58 +0900

    [#46]CartMapperTests 작성

--- src/test/java/com/hyundai/dutyfree/mapper/CartMapperTests.java
+++ src/test/java/com/hyundai/dutyfree/mapper/CartMapperTests.java
@@ -4,6 +4,18 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * CartMapperTests
+ * 
+ * @author 박진수
+ * @since 01.15
+ * 
+ *        <pre>
+ * 수정일                 수정자                                  수정내용
+ * ----------  ---------------    ---------------------------
+ * 2023.01.15   박진수                               최초 생성
+ *        </pre>
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml","file:src/main/webapp/WEB-INF/spring/appServlet/security-context.xml" })
 public class CartMapperTests {
